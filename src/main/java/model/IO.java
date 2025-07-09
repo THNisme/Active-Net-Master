40d53a25b1eb0cd2ec6f6696f2d0dbe148a7658d
THNisme <nghiath.ce191115@gmail.com>
2025-07-09 23:52:08 +0700

    Modify IO.java to add hashMD5 method

--- src/main/java/model/IO.java
+++ src/main/java/model/IO.java
@@ -1,14 +1,36 @@
 package model;
 
+import java.security.MessageDigest;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
- * @author BACH YEN
+ * @author Tran Hieu Nghia - CE191115
  */
 public class IO {
+
+    public static String hashMD5(String pass) {
+        try {
+            MessageDigest mes = MessageDigest.getInstance("MD5");
+            byte[] mesMD5 = mes.digest(pass.getBytes());
+            //[0x0a, 0x7a, 0x12, 0x09,...]
+            StringBuilder str = new StringBuilder();
+            for (byte b : mesMD5) {
+                //0x0a
+                String ch = String.format("%02x", b);
+                //0a
+                str.append(ch);
+            }
+            //str = 0a7a1209
+            return str.toString();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return "";
+    }
+    
     
 }
