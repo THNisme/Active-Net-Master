519c5d777f4fc5ca16744a65baa722b5aef6f5ff
Thungan0307 <nganndt.ce190661@gmail.com>
2025-07-21 00:11:41 +0700

    Create Home Servlet, update home.jsp and LoginServlet

--- src/main/java/controller/HomeServlet.java
+++ src/main/java/controller/HomeServlet.java
@@ -4,7 +4,8 @@
  */
 package controller;
 
-import dao.UserDAO;
+import dao.ProductDAO;
+import dao.TicketsDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -13,16 +14,18 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
-import model.IO;
+import model.Product;
+import model.Ticket;
 import model.User;
 
 /**
  *
- * @author BACH YEN
+ * @author Admin
  */
-@WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
-public class LoginServlet extends HttpServlet {
+@WebServlet(name = "HomeServlet", urlPatterns = {"/home"})
+public class HomeServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -41,10 +44,10 @@ public class LoginServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");
+            out.println("<title>Servlet HomeServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet HomeServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -62,7 +65,19 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("login.jsp").forward(request, response);
+
+        
+        TicketsDAO ticketDAO = new TicketsDAO();
+        ProductDAO productDAO = new ProductDAO();
+        
+        List<Ticket> listTicket = ticketDAO.getAll();
+        List<Product> listProduct = productDAO.getAll();
+        
+        request.setAttribute("listTicket", listTicket);
+        request.setAttribute("listProduct", listProduct);
+        
+        request.getRequestDispatcher("home.jsp").forward(request, response);
+
     }
 
     /**
@@ -76,28 +91,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String user = request.getParameter("user");
-        String pass = request.getParameter("pass");
-
-        UserDAO dao = new UserDAO();
-        User u = dao.login(user, pass);
-        
-
-        if (u.getId() != -1) {
-            HttpSession session = request.getSession();
-            session.setAttribute("user", u);
-            response.sendRedirect("home");
-            
-        } else {
-//            List<String> errorList = IO.userLoginValidator(user, pass);
-//            request.setAttribute("errorList", errorList);
-//            request.getRequestDispatcher("login").forward(request, response);
-            
-            HttpSession session = request.getSession();
-            session.setAttribute("err", "Email or password is incorrect !");
-            response.sendRedirect("login");
-        }
-
+        processRequest(request, response);
     }
 
     /**
